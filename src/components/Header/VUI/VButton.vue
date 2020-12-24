a56ee2b5e217a08e871f4c010873b17a600ce369
Артур Клювиткин <LeBars@github.com>
2020-12-24 21:48:46 +0500

    add v-btn

---
+++ src/components/Header/VUI/VButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="v-button">
+    <a :href="link">Кнока</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: 'Button'
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+
+</style>
